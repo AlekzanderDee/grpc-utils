679aaeed91d5913e16cc46cd0f0171ac666af360
AlekzanderDee <adultsev@gmail.com>
2018-04-15 17:35:48 -0700

    gRPC server util

---
+++ server/grpc.py
@@ -0,0 +1,24 @@
+from concurrent import futures
+import time
+import grpc
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class GRPCServer:
+
+    def __init__(self, max_workers=10):
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+
+    def register_service(self, add_handler, service_class):
+        add_handler(service_class(), self.server)
+
+    def serve_insecure(self, address='[::]', port=50051):
+        self.server.add_insecure_port('{}:{}'.format(address, port))
+        self.server.start()
+        try:
+            while True:
+                time.sleep(_ONE_DAY_IN_SECONDS)
+        except KeyboardInterrupt:
+            self.server.stop(0)
