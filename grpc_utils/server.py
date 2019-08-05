5deef80a923936f06a704b47e9d025ebe8b36493
Alexander Dultsev <alexander.dultsev@synthego.com>
2019-08-05 11:07:53 -0700

    v0.2.0 Pass extra parameters to the gRPC server constructor

--- grpc_utils/server.py
+++ grpc_utils/server.py
@@ -9,8 +9,14 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 class GRPCServer:
     """Base gRPC server manager"""
 
-    def __init__(self, max_workers=10):
-        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+    def __init__(self, max_workers=10, handlers=None, interceptors=None, options=None, maximum_concurrent_rpcs=None):
+        self.server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=max_workers),
+            handlers=handlers,
+            interceptors=interceptors,
+            options=options,
+            maximum_concurrent_rpcs=maximum_concurrent_rpcs
+        )
 
     def register_service(self, add_handler, service_class):
         add_handler(service_class(), self.server)
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.4',  # Required
+    version='0.2.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -133,7 +133,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'grpcio>=1.11.0',
+        'grpcio>=1.22.0',
     ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
